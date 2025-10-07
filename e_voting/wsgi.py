e2fd2af9a5751c8467fb6805311489a8c0701151
ramor123 <smackthat14321@gmail.com>
2025-10-07 11:15:11 +0800

    Fix folder structure for Render

--- e_voting/wsgi.py
+++ e_voting/wsgi.py
@@ -1,107 +1,16 @@
-from pathlib import Path
+"""
+WSGI config for e_voting project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
+"""
+
 import os
 
-# Build paths inside the project
-BASE_DIR = Path(__file__).resolve().parent.parent
+from django.core.wsgi import get_wsgi_application
 
-# SECURITY
-SECRET_KEY = '%6lp_p!%r$7t-2ql5hc5(r@)8u_fc+6@ugxcnz=h=b(fn#3$p9'
-DEBUG = True  # Turn False in production
-ALLOWED_HOSTS = ['*']
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'e_voting.settings')
 
-# Installed apps
-INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-
-    # Custom apps
-    'account.apps.AccountConfig',
-    'voting.apps.VotingConfig',
-    'administrator.apps.AdministratorConfig',
-]
-
-# Middleware
-MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',  # Added WhiteNoise
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'account.middleware.AccountCheckMiddleWare',
-]
-
-# URLs and templates
-ROOT_URLCONF = 'e_voting.urls'
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['voting/templates', 'administrator/templates'],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-                'voting.context_processors.ElectionTitle',
-            ],
-        },
-    },
-]
-
-WSGI_APPLICATION = 'e_voting.wsgi.application'
-
-# Database (SQLite example, switch to PostgreSQL for production if needed)
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-# Password validation
-AUTH_PASSWORD_VALIDATORS = [
-    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',},
-    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',},
-    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',},
-    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',},
-]
-
-# Internationalization
-LANGUAGE_CODE = 'en-us'
-TIME_ZONE = 'UTC'
-USE_I18N = True
-USE_L10N = True
-USE_TZ = True
-
-# Static files (CSS, JavaScript, Images)
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # For collectstatic
-
-# WhiteNoise: automatically serve static files in production
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-# Media files
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
-# Custom user model
-AUTH_USER_MODEL = 'account.CustomUser'
-AUTHENTICATION_BACKENDS = ['account.email_backend.EmailBackend']
-
-# Election title file
-ELECTION_TITLE_PATH = os.path.join(BASE_DIR, 'election_title.txt')
-
-# OTP toggle
-SEND_OTP = False  # If False, use 0000 as OTP
-
-# Default primary key field type (to remove W042 warnings)
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+application = get_wsgi_application()
